6b1bbf8660894b0ca446ea20365185289d75a02d
slyddd <dayvidnavarrete@gmail.com>
2025-05-10 18:26:16 -0500

    Set sddm theme and enable hyprland

--- modules/system/services/xserver.nix
+++ modules/system/services/xserver.nix
@@ -1,12 +1,30 @@
+{ pkgs, ... }:
+let
+  sddm-astronaut = pkgs.sddm-astronaut.override {
+    embeddedTheme = "black_hole";
+  };
+in
 {
   services.xserver = {
     enable = true;
 
     desktopManager.cinnamon.enable = true;
-    displayManager.lightdm.enable = true;
-
     xkb.layout = "latam";
   };
 
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+    theme = "sddm-astronaut-theme";
+    package = pkgs.kdePackages.sddm;
+    extraPackages = with pkgs; [ kdePackages.qtmultimedia ];
+  };
+
+  environment.systemPackages = [
+    sddm-astronaut
+  ];
+
   services.libinput.enable = true;
+
+  programs.hyprland.enable = true;
 }
