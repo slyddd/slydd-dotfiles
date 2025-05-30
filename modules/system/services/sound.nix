1fe8e855546a03ffd93c356a0ecfd4243d593b53
slyddd <dayvidnavarrete@gmail.com>
2025-05-30 11:43:22 -0500

    Add pipewire sound settings

--- modules/system/services/sound.nix
+++ modules/system/services/sound.nix
@@ -1,6 +1,11 @@
+{ pkgs, ... }:
 {
   services.pipewire = {
     enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    audio.enable = true;
+    jack.enable = true;
     pulse.enable = true;
     wireplumber = {
       enable = true;
