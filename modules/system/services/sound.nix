818d400ee8597002e60965a1d7a8819df42dcd6d
slyddd <dayvidnavarrete@gmail.com>
2025-05-30 11:43:08 -0500

    Add pipewire sound settings

--- modules/system/services/sound.nix
+++ modules/system/services/sound.nix
@@ -2,5 +2,18 @@
   services.pipewire = {
     enable = true;
     pulse.enable = true;
+    wireplumber = {
+      enable = true;
+      configPackages = [
+        (pkgs.writeTextDir "share/bluetooth.lua.d/51-bluez-config.lua" ''
+          bluez_monitor.properties = {
+            ["bluez5.enable-sbc-xq"] = true,
+            ["bluez5.enable-msbc"] = true,
+            ["bluez5.enable-hw-volume"] = true,
+            ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+          }
+        '')
+      ];
+    };
   };
 }
