f0232af8d29483b25d7659a463a497ea74c3a28d
slyddd <dayvidnavarrete@gmail.com>
2026-04-05 23:28:16 -0500

    Add remote disks

--- hosts/dev2die/disk.nix
+++ hosts/dev2die/disk.nix
@@ -1,7 +1,6 @@
 { ... }:
 {
-  boot.initrd.luks.devices."nixos".device =
-    "/dev/disk/by-uuid/556b5dea-b11e-471e-a01d-03dc98dd9d29";
+  boot.initrd.luks.devices."nixos".device = "/dev/disk/by-uuid/556b5dea-b11e-471e-a01d-03dc98dd9d29";
 
   fileSystems."/" = {
     device = "/dev/mapper/nixos";
@@ -30,7 +29,48 @@
   fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/B15B-36C6";
     fsType = "vfat";
-    options = [ "fmask=0022" "dmask=0022" ];
+    options = [
+      "fmask=0022"
+      "dmask=0022"
+    ];
+  };
+
+  fileSystems."/home/serv2die" = {
+    device = "slydd@192.168.10.3:/home/slydd";
+    fsType = "sshfs";
+    options = [
+      "nodev"
+      "noatime"
+      "allow_other"
+      "_netdev"
+      "reconnect"
+      "ServerAliveInterval=15"
+      "IdentityFile=/root/.ssh/id_ed25519"
+    ];
+  };
+
+  fileSystems."/home/minecraft" = {
+    device = "sbhfhk5p.6fa1d023@urdusin.ultraservers.com:/";
+    fsType = "sshfs";
+    options = [
+      "_netdev"
+      "x-systemd.automount"
+      "nofail"
+      "reconnect"
+      "ServerAliveInterval=15"
+      "Port=60002"
+      "IdentityFile=/root/.ssh/id_ed25519"
+      "UserKnownHostsFile=/root/.ssh/known_hosts"
+      "StrictHostKeyChecking=accept-new"
+      "allow_other"
+      "default_permissions"
+
+      "workaround=fstat"
+
+      "uid=1000"
+      "gid=100"
+      "umask=0022"
+    ];
   };
 
   swapDevices = [ ];
