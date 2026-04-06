249b9a445fea6f06254949f576fe8fc1406c32f4
slyddd <dayvidnavarrete@gmail.com>
2026-04-05 23:33:46 -0500

    Add input flakes

--- flake.nix
+++ flake.nix
@@ -2,13 +2,16 @@
   description = "Slydd dotfiles using flakes";
 
   inputs = {
+    # Nixpks in latest version
     nixpkgs.url = "github:NixOS/nixpkgs";
 
+    # Home Manager in latest version, following nixpkgs
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # Zen Browser, with home-manager support
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs = {
@@ -16,17 +19,24 @@
         home-manager.follows = "home-manager";
       };
     };
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
+    # Git hooks for nix
     git-hooks = {
       url = "github:cachix/git-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # SDDM modified
     silentSDDM = {
       url = "github:uiriansan/SilentSDDM";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # Dank Material Shell, with home-manager support
     dms = {
       url = "github:AvengeMedia/DankMaterialShell";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -35,22 +45,43 @@
       url = "github:AvengeMedia/dms-plugin-registry";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-  };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, ... }:
-  let
-    system = "x86_64-linux";
-  in {
-    nixosConfigurations.DEV2DIE = nixpkgs.lib.nixosSystem {
-      inherit system;
-      specialArgs = { inherit inputs; };
+    # NvChad to nvim
+    nix4nvchad = {
+      url = "github:nix-community/nix4nvchad";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-      modules = [
-        ./system
-        ./hosts/dev2die
-        ./home
-        home-manager.nixosModules.home-manager
-      ];
+    # Decky Loader for Steam Deck, with home-manager support
+    jovian = {
+      url = "github:Jovian-Experiments/Jovian-NixOS";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      home-manager,
+      jovian,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      nixosConfigurations.DEV2DIE = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit inputs; };
+
+        modules = [
+          ./system
+          ./hosts/dev2die
+          ./home
+          home-manager.nixosModules.home-manager
+          jovian.nixosModules.default
+        ];
+      };
+    };
 }
