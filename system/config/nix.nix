66d018b18507cb35fc5ac39e2f270d64dc86fe01
slyddd <dayvidnavarrete@gmail.com>
2026-04-05 23:29:18 -0500

    Set Garbage Collector in Nix

--- system/config/nix.nix
+++ system/config/nix.nix
@@ -1,12 +1,27 @@
-{...}:
+{ ... }:
 {
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "daily";
+      options = "--delete-older-than 10d";
+    };
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+    };
+  };
 
   nixpkgs.config = {
     allowUnfree = true;
     android_sdk.accept_license = true;
+  };
+
+  system.autoUpgrade = {
+    enable = true;
+    dates = "weekly";
   };
 }
